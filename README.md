73b76c97b5e758b0049488fc8ce39ef84aa5eeb0
hkust_git <lmwongad@connect.ust.hk>
2025-08-12 13:27:37 +0800

    Edit on READNE.md file

--- README.md
+++ README.md
@@ -4,13 +4,13 @@
 1. Clone this repository to your own computer
 2. Remove the original remote origin of this repository in your own computer 
 3. Merge `study` and `play` branch to `master` branch
-4. Enter your personal information at the end of the `README.md` file
+4. Enter your personal information (Name and ITSC) at the end of the `README.md` file
 5. Run the `main.py` file and screenshot the output, and put your screenshot at the end of this `README.md` file
-6. Replace the photo below to the screenshot you have just capture 
+6. Replace the photo below to the screenshot you have just capture (You can keep the photo if you want) 
 7. Make a repository name `HKUST-COMP4900-<Your ITSC>` on GitHub (e.g `HKUST-COMP4900-lmwongad`) (Remember to make it public)
 8. Add the remote repository you have made to your local repository
-9. Push your work to your renote repository
-10. Paste your repository work to the canvas for hand in
+9. Push your work to your own remote repository
+10. Paste your remote repository link to canvas for hand in
 
 (You can refer [here](https://github.com/hkustGIT/COMP4900-Workshop-Task) for how to do the task)
 (The link will be update in later push)
--- main.py
+++ main.py
@@ -1,16 +1,36 @@
+# import the 'activities' from corresponding python file
 from study import study
 from play import play
 
+def printLine():
+    print("="*50)
 # main program start
 if __name__ == "__main__":
+
+    printLine()
+    print("Today is a wonderful day!")
+
+    # Make plan for today
+    printLine()
+    print("What should I aim for today?")
     plan1 = study()
     plan2 = play()
 
+    # Work on the plan
+    printLine()
+    print("And is time to work on the plan, and...")
     do_plan1 = plan1.spend_time()
     do_plan2 = plan2.spend_time()
     
-    if do_plan1 and do_plan2:
-        print("I trust nessie, therefore:")
-        print("I have work life balance :)")
+    # End of today
+    printLine()
+    print("Is almost the end of today, and ". end='')
+    if do_plan1 and do_plan2:                           # Successfully merge two branch
+        print("I have work life balance!!!")
+        print("Thanks for doing merge on my 'study' and 'play' branch to 'master' branch :)))")
     else:
-        print("Why I can't have work life balance")
+        print("Why I can't have work life balance :(")  # Didn't merge for two branch
+        print("I should do the merge my 'study' and 'play' branch to 'master' branch and make my life better")
+
+    printLine()
+    print("Btw Nessie is cute :3")                      # Random Nessie Appear
