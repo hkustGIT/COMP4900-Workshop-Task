8ade80db2d476549b514ee2651493cb3160633db
hkust_git <lmwongad@connect.ust.hk>
2025-08-12 13:12:22 +0800

    Add comment and fix typo for play.py

--- play.py
+++ play.py
@@ -1,9 +1,11 @@
 from activities import activities
 
+# play class is inherit of activities class
 class play(activities):
     def __init__(self):
         super().__init__("play")
 
+    # We have time to play so return True
     def spend_time(self) -> bool:
-        print("I have time to play :(")
+        print("I have time to play :)")
         return True
