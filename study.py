00a5a1f3d1ffc78ce0c1606462c7728d22ebfaca
hkust_git <lmwongad@connect.ust.hk>
2025-08-12 13:13:29 +0800

    Add comment and fix typo for study.py

--- study.py
+++ study.py
@@ -1,9 +1,11 @@
 from activities import activities
 
+# study class is inherit from study class
 class study(activities):
     def __init__(self):
         super().__init__("study")
     
+    # We have time to study so return True
     def spend_time(self) -> bool:
-        print("I have time to study :(")
+        print("I have time to study :)")
         return True
